41762c2b71ee8cde1d6acc1e3da339fdc4e9d426
school <school>
2016-09-21 17:14:59 -0700

    started basics

--- Week01-ObjectBasicsJasmine/Source/MyObject.js
+++ Week01-ObjectBasicsJasmine/Source/MyObject.js
@@ -1,1 +1,20 @@
+var myObject = {
+	num : 80,
+	'strings': 'sdkfj',
+	getOne: function() {
+        'use strict';
+        return 1;
+    },
+    getTwo: function() {
+        'use strict';
+        return 2;
+    },
+    getThree: function() {
+        'use strict';
+        return 3;
+    }
+	
+	
+};
+
 exports.myObject;
