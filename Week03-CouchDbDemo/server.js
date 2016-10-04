4710966ff92433fa50b64a31fd6b4e3b60fb7162
school <school>
2016-10-03 17:11:53 -0700

    started couch

---
+++ Week03-CouchDbDemo/server.js
@@ -0,0 +1,34 @@
+// var nano = require('nano')('http://localhost:5984');
+var nano = require('nano')('http://168.156.47.113:5984');
+
+var docName = 'bigNames';
+var dbName = 'bc_data';
+
+var readIt = function() {
+	var prog = nano.db.use(dbName);
+	prog.get(docName, { revs_info: true }, function(err, body) {
+		if (!err)
+			console.log(body);
+	});
+}
+
+function insert() {
+	nano.db.create(dbName);
+	var prog = nano.db.use(dbName);
+
+	prog.insert({"doc": [ 
+		{"firstName": "Suzie", "lastName": "Higgins"},
+		{"firstName": "Harry", "lastName": "Potter"},
+		{"firstName": "Suz", "lastName": "asdfsdf"}
+		]}, 
+		docName, function(err, body) {
+	  if (!err)
+		console.log(body);
+		readIt();
+	});
+}
+
+
+insert();
+
+// readIt();
