e415c2db1d884ab8e244e66499ce7ef2da0c4ec3
school <school>
2016-10-05 17:15:56 -0700

    started express routes

---
+++ Week03-ExpressRoutes/public/javascripts/control.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bcuser on 10/5/16.
+ */
+
+$(document).ready(function() {
+    console.log('Document loaded in prog272');
+
+    $('#read').click(read);
+
+    function read() {
+        console.log('callRead called');
+    }
+});
