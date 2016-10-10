81ffb4d26470b3e0f085940453f4f948fe804b6d
school <school>
2016-10-10 16:29:19 -0700

    up threejsbasics

---
+++ Week04-ThreeJsBasics/public/javascripts/control.js
@@ -0,0 +1,50 @@
+/* globals define: true, THREE:true */
+
+define(function() {
+
+    var scene = null;
+    var camera = null;
+    var renderer = null;
+    var cube = null;
+    var THREE = null;
+
+    function Control(threeInit) {
+        THREE = threeInit;
+        console.log("Control called");
+        scene = new THREE.Scene();
+        var width = window.innerWidth / window.innerHeight;
+        camera = new THREE.PerspectiveCamera(75, width, 0.1, 1000);
+        renderer = new THREE.WebGLRenderer({
+            antialias : true
+        });
+        renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+        document.body.appendChild(renderer.domElement);
+        cube = addCube(scene, camera, false, 1, 1);
+        camera.position.z = 23;
+        camera.position.x = 2;
+        camera.position.y = 0;
+        render();
+    }
+
+    function render() {
+        requestAnimationFrame(render);
+        cube.rotation.x += 0.05;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    }
+
+    function addCube(scene, camera, wireFrame, x, y) {
+        var geometry = new THREE.BoxGeometry(7, 7, 7);
+        var material = new THREE.MeshNormalMaterial({
+            color : 0x00ffff,
+            wireframe : wireFrame
+        });
+        var cube = new THREE.Mesh(geometry, material);
+        cube.position.set(x, 0, y);
+        scene.add(cube);
+
+        return cube;
+    }
+
+    return Control;
+});
---
+++ Week04-ThreeJsBasics/public/javascripts/main.js
@@ -0,0 +1,28 @@
+/**
+ * Created by bcuser on 10/10/16.
+ */
+
+requirejs.config({
+    baseUrl : '.',
+    paths : {
+        "jquery" : 'components/jquery/dist/jquery',
+        "bootstrap": 'components/bootstrap/dist/js/bootstrap',
+        "Three" : 'javascripts/three',
+        "control" : 'javascripts/control'
+
+    },
+    shim : {
+        'Three' : {
+            exports: 'THREE'
+        }
+    }
+});
+
+requirejs([ 'jquery'], function($) {
+    'use strict';
+    requirejs(['bootstrap', 'Three', 'control' ], function(bootstrap, THREE, Control) {
+        $(document).ready(function() {
+            var control = new Control(THREE);
+        });
+    });
+});
