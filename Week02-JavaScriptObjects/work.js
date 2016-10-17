d5094f59e9fc3b9fe0dfa8cd3b390a5be8718f09
Sil at home <meh@example.com>
2016-10-17 02:25:50 -0700

    finished javascriptobjects

--- Week02-JavaScriptObjects/work.js
+++ Week02-JavaScriptObjects/work.js
@@ -1,17 +1,48 @@
 var Person = {
-	firstName: "George",
-	lastName: "Washington",
-	fullName: function (){
-		return this.firstName + " " + this.lastName;
-	}
+    firstName: 'George',
+    lastName: 'Washington',
+    fullName: function() {
+        'use strict';
+        return this.firstName + ' ' + this.lastName;
+    }
+};
+
+function divider(title) {
+    'use strict';
+    console.log('====================================');
+    console.log(title);
+    console.log('====================================');
 }
-	
-console.log(Person.firstName);	
+
+var calculator = {
+    operator01: -1,
+    operator02: -1,
+    add: function() {
+        'use strict';
+        return this.operator01 + this.operator02;
+    },
+    subtract: function() {
+        'use strict';
+        return this.operator01 - this.operator02;
+    },
+    multiply: function() {
+        'use strict';
+        return this.operator01 * this.operator02;
+    }
+};
+
+calculator.operator01 = Person.firstName.length;
+calculator.operator02 = Person.lastName.length;
+
+divider('Person');
+console.log(Person.firstName);
 console.log(Person.lastName);
 
 console.log(Person.fullName());
 
-
-
-
-
+divider('Calculator');
+console.log('operator01 = ' + calculator.operator01);
+console.log('operator02 = ' + calculator.operator02);
+console.log('Add: = ' + calculator.add());
+console.log('Subtract = ' + calculator.subtract());
+console.log('Multiply = ' + calculator.multiply());
