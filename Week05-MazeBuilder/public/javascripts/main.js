5c39d50f8d3aa91e897ded4cb7c845f0472a655b
school <school>
2016-10-17 17:25:51 -0700

    started mazebuilder

---
+++ Week05-MazeBuilder/public/javascripts/main.js
@@ -0,0 +1,30 @@
+/**
+ * Created by bcuser on 10/10/16.
+ */
+
+requirejs.config({
+    baseUrl: '.',
+    paths: {
+        "jquery": 'components/jquery/dist/jquery',
+        "Three": 'javascripts/three',
+        "control": 'javascripts/control',
+        "Floors": 'javascripts/floors',
+        "PointerLockControls": 'javascripts/pointer-lock-controls',
+        "PointerLockSetup": 'javascripts/pointer-lock-setup'
+
+    },
+    shim: {
+        'Three': {
+            exports: 'THREE'
+        }
+    }
+});
+
+requirejs(['jquery'], function($) {
+    'use strict';
+    requirejs(['Three', 'control'], function(THREE, Control) {
+        $(document).ready(function() {
+            var control = new Control(THREE);
+        });
+    });
+});
