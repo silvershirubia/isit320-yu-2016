0c3cf8a6bab5601a3faee69c1c5d883b5a4d164d
school <school>
2016-12-07 16:36:53 -0800

    changed ip

---
+++ DataMaster/public/javascripts/name-controller.js
@@ -0,0 +1,37 @@
+/**
+ * Created by bcuser on 11/2/16.
+ */
+define(['runQuery'], function(runQuery) {
+    'use strict';
+
+    var nameController = function(query, data) {
+        var debug = $('#debug');
+        var docs = $('#docs');
+        // YOU WRITE THE REST OF THE CODE
+        // YOU NEED TO HANDLE WHAT HAPPENS WHEN
+        // EITHER THE databaseName METHOD IS CALLED
+        // OR WHEN THE allDbs METHOD IS CALLED
+        // VERY SIMILAR TO queryController, but simpler.
+        if (query === '/databaseName') {
+            debug.html(data.currentDatabaseName);
+        } else if (query === '/allDbs') {
+            debug.html('here');
+        }else if (query === '/readOne') {
+            debug.html('here');
+        }
+    };
+
+    nameController.databaseName = function($q) {
+        return runQuery('/databaseName', $q);
+    };
+
+    nameController.allDbs = function($q) {
+        return runQuery('/listDb', $q);
+    };
+
+    nameController.readOne = function($q) {
+        return runQuery('/readOne', $q);
+    };
+
+    return nameController;
+});
