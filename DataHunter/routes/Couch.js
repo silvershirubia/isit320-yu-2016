6510d51a5887b509924d4a5f5c900f7fecaa1d8f
Ubuntu <ubuntu@ip-172-31-21-20.us-west-2.compute.internal>
2016-12-09 00:33:54 +0000

    added service files

--- DataHunter/routes/Couch.js
+++ DataHunter/routes/Couch.js
@@ -7,7 +7,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-var servers = ['http://35.163.116.83:5984', 'http://192.168.2.19:5984', 'http://192.168.0.17:5984', 'http://168.156.41.65:5984'];
+var servers = ['http://127.0.0.1:5984', 'http://192.168.2.19:5984', 'http://192.168.0.17:5984', 'http://168.156.41.65:5984'];
 var serverIndex = 0;
 var nano = require('nano')(servers[serverIndex]);
 
--- DataMaster/routes/Couch.js
+++ DataMaster/routes/Couch.js
@@ -8,7 +8,7 @@ var router = express.Router();
 var fs = require('fs');
 
 var servers = ['http://127.0.0.1:5984', 'http://192.168.2.19:5984', 'http://168.156.47.55:5984'];
-var serverIndex = 2;
+var serverIndex = 0;
 var nano = require('nano')(servers[serverIndex]);
 
 var dbName = 'couch-views-yu';
